1099dfc1133aa1107d76cb86b0ae1e2597cf543a
szabi.keresztes <szabi.keresztes@bjss.com>
2020-02-22 14:22:45 +0000

    add new habits and perform habits

--- src/App.js
+++ src/App.js
@@ -1,26 +1,89 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      newHabit: {
+        name: '',
+      },
+      habits: [{
+        name: 'cycling',
+      }, {
+        name: 'drawing',
+      }, {
+        name: 'exercise',
+      }, {
+        name: 'cooking',
+      }],
+      performedHabits: [],
+    };
+  }
+
+  addHabit = () => {
+    if (this.state.newHabit.name === '') {
+      return;
+    }
+
+    this.setState({
+      habits: [
+        ...this.state.habits,
+        this.state.newHabit,
+      ]
+    })
+  }
+
+  habitNameChange = (event) => {
+    this.setState({
+      newHabit: {
+        name: event.target.value,
+      },
+    });
+  }
+
+  performHabit = (habit) => {
+    this.setState({
+      performedHabits: [
+        ...this.state.performedHabits,
+        {
+          habit: habit,
+          timestamp: Date.now(),
+        },
+      ],
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div id="list-of-performed-habits">
+          {this.state.performedHabits.map((performedHabit, index) => (
+            <div key={index}>[{performedHabit.timestamp}]: {performedHabit.habit.name}</div>
+          ))}
+        </div>
+        <div id="tool-bar">
+          <p>
+            My Habit Tracker
+          </p>
+          <div>
+            <input placeholder="habit name" value={this.state.newHabit.name} onChange={this.habitNameChange}/> 
+          </div>
+          <div>
+            <button id="add-habit-button" onClick={this.addHabit}>Add Habit</button>
+          </div>
+          <div id="list-of-habits-header">List of Habits:</div>
+          <div id="list-of-habits">
+            {this.state.habits.map((habit, index) => (
+              <button className="habit-button" key={index} onClick={() => this.performHabit(habit)}>{habit.name}</button>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
